d8bdfb0c0c20d8a18c03a6e11838ae48da699b01
zGodless <952715899@qq.com>
2019-05-10 23:52:47 +0800

    view学习（部分）

---
+++ MvcViewApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcViewApp.Models;
+
+namespace MvcViewApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["One"] = "天机老人";
+            ViewBag.Two = "bbbbbb";
+            var _user = new User {Name = "ccccc"};
+            TempData["Four"] = "ddddd";
+            return View(_user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
