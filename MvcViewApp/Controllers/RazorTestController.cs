f8a5cb02ae3ebce85495fc83d06660b6e62c7356
zGodless <952715899@qq.com>
2019-05-14 01:11:28 +0800

    完成view高级

---
+++ MvcViewApp/Controllers/RazorTestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcViewApp.Controllers
+{
+    public class RazorTestController : Controller
+    {
+        // GET: RazorTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialViewTest()
+        {
+            ViewData["Msg"] = "this is a message";
+            return PartialView();
+        }
+
+        public ActionResult ActionTest()
+        {
+            return View();
+        }
+    }
+}
--- MvcViewApp/Global.asax.cs
+++ MvcViewApp/Global.asax.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StackExchange.Profiling;
+using StackExchange.Profiling.EntityFramework6;
+using StackExchange.Profiling.Mvc;
+
 
 namespace MvcViewApp
 {
@@ -16,6 +20,40 @@ namespace MvcViewApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+           
+             MiniProfilerEF6.Initialize();
+             MiniProfiler.Configure(new MiniProfilerOptions
+             {
+                 RouteBasePath = "~/profiler",
+                 PopupRenderPosition = RenderPosition.Right,  // defaults to left
+                 PopupMaxTracesToShow = 10,
+                 ResultsAuthorize = request => request.IsLocal,
+                 ResultsListAuthorize = request =>
+                 {
+                     return true; // all requests are legit in this example
+                 },
+                 StackMaxLength = 256, 
+                 TrackConnectionOpenClose = true
+             }
+                 .ExcludeType("SessionFactory")  // Ignore any class with the name of SessionFactory)
+                 .ExcludeAssembly("NHibernate")  // Ignore any assembly named NHibernate
+                     .ExcludeMethod("Flush") // Ignore any method with the name of Flush
+                     .AddViewProfiling()
+             );
+              MiniProfilerEF6.Initialize();
+
+        }
+        protected void Application_BeginRequest(Object source, EventArgs e)
+        {
+            if (Request.IsLocal) // Example of conditional profiling, you could just call MiniProfiler.StartNew();
+            {
+                MiniProfiler.StartNew();
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Current?.Stop(); // Be sure to stop the profiler!
         }
     }
 }
